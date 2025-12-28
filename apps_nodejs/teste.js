605b70e47ecaac651c7f77309458b7d5820ed81b
mbarbiero <mbarbiero@hotmail.com>
2025-12-28 08:24:10 -0300

    28/12/2025

---
+++ apps_nodejs/teste.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // So Express know you're using JSON
+
+app.get('/', (request, response) => {
+  return 'Hello World!';
+});
+
+// POST a JSON object and get it back
+app.post('/demo-object', (request, response) => {
+
+  const body = request.body;
+
+  return body;
+});
+
+// GET with the id and get it back
+app.get('/demo-object/:id', (request, response) => {
+  
+  const params = request.params;
+
+  return response.json(params);
+});
+
+// GET with query in the URI and get it back
+app.get('/demo-object', (request, response) => {
+  
+  const params = request.query;
+
+  return response.json(query);
+});
+
+
+
+app.listen(3000);
